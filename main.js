f55b50e0dfc160a97e93fc7a08cd7988247d827a
shawn-don-soneja <63383987+shawn-don-soneja@users.noreply.github.com>
2020-04-09 00:43:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-function changeMe(){
-  var teste = document.getElementById("testMe");
-  teste.style.color = 'red';
-}
-
-var tester = document.getElementById("testMe");
-tester.onClick = function(){changeMe()};
-
-//document.getElementById("demo").addEventListener("click", myFunction);
+document.getElementById("testTwo").style.background = 'red';
