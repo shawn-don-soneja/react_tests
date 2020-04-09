9ebd4db0b348976fadfdd71565961ccbfa640f33
shawn-don-soneja <63383987+shawn-don-soneja@users.noreply.github.com>
2020-04-09 00:53:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-document.getElementById("testTwo").style.background = 'red';
+//document.getElementById("testTwo").style.background = 'red';
+function myFunction(arg){
+  arg.style.background = 'red';
+}
